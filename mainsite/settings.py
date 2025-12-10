866dec6fea1217bd4133aba1b11861009000b679
Matthew R. <git@mro.land>
2025-12-09 23:13:32 -0600

    add environment variables

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tiu$t*)cnqex7s_k0^bvs=9m_v)rr2dt-ikoupzd5ok=&)vqa0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'FALSE') == 'TRUE'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('DOMAIN', 'localhost')]
 
 
 # Application definition
