bacc7d79e52927118e6308f2b02e114753474ada
Matthew R <git@mro.land>
2025-12-13 23:16:59 -0500

    feat!: Change from static website to Django framework (#1)
    * add django
    
    * add whitenoise
    
    * add tailwind
    
    * implement docker
    
    
    BREAKING CHANGES:
    Website is no longer static and uses django framework

---
+++ Dockerfile
@@ -0,0 +1,61 @@
+ARG BASE_IMAGE="python:3.13-slim"
+
+FROM ${BASE_IMAGE} AS installer
+ENV PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONUNBUFFERED=1
+
+RUN pip install --upgrade pip
+COPY requirements.txt requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+
+FROM ${BASE_IMAGE} AS builder
+WORKDIR /app
+ENV PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONUNBUFFERED=1
+
+COPY --from=installer \
+    /usr/local/lib/python3.13/site-packages/ \
+    /usr/local/lib/python3.13/site-packages/
+COPY --from=installer \
+    /usr/local/bin/ \
+    /usr/local/bin/
+
+COPY manage.py /app/manage.py
+COPY mainsite /app/mainsite/
+COPY theme /app/theme
+RUN python manage.py tailwind install --no-package-lock
+RUN python manage.py tailwind build
+RUN python manage.py collectstatic --no-input
+RUN python manage.py migrate --no-input
+
+FROM ${BASE_IMAGE} AS final
+WORKDIR /app
+
+ENV PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONUNBUFFERED=1
+
+RUN useradd -m -r appuser
+
+COPY --from=installer \
+    /usr/local/lib/python3.13/site-packages/ \
+    /usr/local/lib/python3.13/site-packages/
+COPY --from=installer \
+    /usr/local/bin/ \
+    /usr/local/bin/
+COPY --from=builder \
+    --chown=appuser:appuser \
+    /app/mainsite /app/mainsite
+COPY --from=builder \
+    --chown=appuser:appuser \
+    /app/theme /app/theme
+COPY --from=builder \
+    --chown=appuser:appuser \
+    /app/static /app/static
+
+USER appuser
+
+EXPOSE 8000
+
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "--workers", "3", "mainsite.wsgi:application", "--access-logfile", "-", "--error-logfile", "-"]
+
---
+++ mainsite/settings.py
@@ -0,0 +1,131 @@
+"""
+Django settings for mainsite project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/6.0/ref/settings/
+"""
+
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-tiu$t*)cnqex7s_k0^bvs=9m_v)rr2dt-ikoupzd5ok=&)vqa0')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
+
+if domain := os.environ.get('DOMAIN'):
+    ALLOWED_HOSTS = [domain]
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'mainsite',
+    'tailwind',
+    'theme',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+]
+
+ROOT_URLCONF = 'mainsite.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'mainsite.vars.global_vars'
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'mainsite.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/6.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+TAILWIND_APP_NAME = "theme"
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/6.0/howto/static-files/
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
