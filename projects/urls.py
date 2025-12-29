f4e19a04a214acef4edabbd48da78f58e7952bdb
Matthew R. <git@mro.land>
2025-12-29 12:37:03 -0600

    change project index url name

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
+
 from projects import views
 
 app_name = "projects"
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='projects/index.html'), name=''),
+    path('', TemplateView.as_view(template_name='projects/index.html'), name='index'),
 ]
 
