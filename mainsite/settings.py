54aa53fe7ab6353834603c240a4123c67d628d70
Matthew R. <git@mro.land>
2025-12-30 09:40:48 -0600

    add partial dep and projects app

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django_components',
+    'template_partials',
     'pygmentify',
 
     'mainsite',
--- projects/urls.py
+++ projects/urls.py
@@ -6,5 +6,9 @@ from projects import views
 app_name = "projects"
 urlpatterns = [
     path('', TemplateView.as_view(template_name='projects/index.html'), name='index'),
+    path('cs2-grading', TemplateView.as_view(template_name='projects/projects/cs2-grading.html'), name='cs2-grading'),
+    path('homelab', TemplateView.as_view(template_name='projects/projects/homelab.html'), name='homelab'),
+    path('installation-troubleshooting', TemplateView.as_view(template_name='projects/projects/installation-troubleshooting.html'), name='installation-troubleshooting'),
+    path('mainsite', TemplateView.as_view(template_name='projects/projects/mainsite.html'), name='mainsite'),
 ]
 
