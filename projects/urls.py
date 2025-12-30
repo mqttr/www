294f056d0b2e8a4bda361883127a0c8b6889d2e0
Matthew R. <git@mro.land>
2025-12-30 10:22:48 -0600

    update project templates

--- projects/urls.py
+++ projects/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 from projects import views
@@ -6,9 +6,9 @@ from projects import views
 app_name = "projects"
 urlpatterns = [
     path('', TemplateView.as_view(template_name='projects/index.html'), name='index'),
-    path('cs2-grading', TemplateView.as_view(template_name='projects/projects/cs2-grading.html'), name='cs2-grading'),
+    path('cs2_grading', TemplateView.as_view(template_name='projects/projects/cs2_grading.html'), name='cs2_grading'),
     path('homelab', TemplateView.as_view(template_name='projects/projects/homelab.html'), name='homelab'),
-    path('installation-troubleshooting', TemplateView.as_view(template_name='projects/projects/installation-troubleshooting.html'), name='installation-troubleshooting'),
-    path('mainsite', TemplateView.as_view(template_name='projects/projects/mainsite.html'), name='mainsite'),
+    path('installation_troubleshooting', TemplateView.as_view(template_name='projects/projects/installation_troubleshooting.html'), name='installation_troubleshooting'),
+    path('website', TemplateView.as_view(template_name='projects/projects/mainsite.html'), name='mainsite'),
 ]
 
